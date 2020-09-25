977fc0c02ad451f331e03e43b5f9e19bc7a52512
marianacoimbra <mariana.coimbra@ccc.ufcg.edu.br>
2020-09-25 01:08:20 -0400

    add anottation

--- src/main/java/com/ufcg/psoft/mercadofacil/repositories/CompraRepository.java
+++ src/main/java/com/ufcg/psoft/mercadofacil/repositories/CompraRepository.java
@@ -3,10 +3,11 @@ package com.ufcg.psoft.mercadofacil.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ufcg.psoft.mercadofacil.model.Compra;
 
-
+@Repository
 public interface CompraRepository extends JpaRepository<Compra, Long>{
 
 }
